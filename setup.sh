a029cf607569312d15d67499bbb822696833956e
Yacov Buchritz <jackbu131@gmail.com>
2019-03-13 14:14:43 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -46,9 +46,9 @@ fi
 
 title "Installing OS packages"
 if `isOsx` ; then
-	brew install python3 node yarn neovim homebrew/cask/pinta
+	brew install node yarn
 else
-	sudo apt install --assume-yes xsel git zsh scrot python3 i3 pinta pavucontrol curl blueman
+	sudo apt install --assume-yes xsel git zsh scrot python3 pinta pavucontrol curl blueman
 
 	if ! hash node 2>/dev/null; then
 		title "Installing NodeJS"
@@ -56,39 +56,18 @@ else
 		sudo apt install --assume-yes nodejs
 	fi
 
-	if ! hash nvim 2>/dev/null; then
-		title "Installing NeoVim"
-		curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
-		chmod u+x nvim.appimage
-		sudo mv nvim.appimage /usr/bin/nvim
-	fi
 fi
-
-title "Installing Python packages"
-pip3 install -U python-language-server pyls-mypy
 
 title "Setting symlinks"
 linkItem "Fonts directory"                            ~/.fonts                           "fonts"
-if `isLinux` ; then
-	linkItem "i3wm configuration"                         ~/.config/i3/config                "dotfiles/i3-config"
-	linkItem "i3wm's status-bar"                          ~/.config/i3/i3status.conf         "dotfiles/i3status-config"
-	linkItem "Custom status script for i3wm's status-bar" ~/.config/i3/my-status.sh          "dotfiles/my-i3status-script.sh"
-	linkItem "Custom status script for i3wm's status-bar" ~/.config/i3/my-status.py          "dotfiles/my-i3status-script.py"
-	linkItem "Custom lock-screen script for i3wm"         ~/.config/i3/my-lockscreen.sh      "scripts/my-i3-lockscreen.sh"
-	linkItem "Custom lock-screen image for i3wm"          ~/.config/i3/lockscreen-center.png "pictures/lockscreen-center.png"
-	#linkItem "Urxvt configuration"                        ~/.Xdefaults                       "dotfiles/Xdefaults"
-fi
+
 #linkItem "tmux configuration"                         ~/.config/tmux/config              "dotfiles/tmux.conf"
-#linkItem "Hyper.app configuration"                    ~/.hyper.js                        "dotfiles/hyper.js"
 linkItem "Git configuration (1/3)"                    ~/.gitconfig                       "dotfiles/gitconfig"
 linkItem "Git configuration (2/3)"                    ~/.gitignore                       "dotfiles/gitignore"
 linkItem "Git configuration (3/3)"                    ~/dev/.gitconfig                   "dotfiles/work-gitconfig"
 linkItem "Zsh configuration"                          ~/.zshrc                           "dotfiles/zshrc"
 linkItem "NPM configuration"                          ~/.npmrc                           "dotfiles/npmrc"
-linkItem "Oni configuration"                          ~/.oni/config.js                   "dotfiles/oni-config.js"
-linkItem "NeoVim configuration"                       ~/.config/nvim/init.vim            "dotfiles/init.vim"
 
 if `isOsx` ; then
 	defaults write -g com.apple.swipescrolldirection -bool FALSE
-	defaults write com.extropy.oni ApplePressAndHoldEnabled -bool false
 fi
